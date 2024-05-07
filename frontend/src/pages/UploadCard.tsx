f051955f1632aa922395dcef393a717a245785e7
kevin.macbook <vb735499@gmail.com>
2024-05-07 16:29:29 +0800

    remove unnecessary import in UploadCard.yml

--- frontend/src/pages/UploadCard.tsx
+++ frontend/src/pages/UploadCard.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { styled } from '@mui/material/styles';
 import UploadForm from './UploadForm';
 import Typography from '@mui/material/Typography';
